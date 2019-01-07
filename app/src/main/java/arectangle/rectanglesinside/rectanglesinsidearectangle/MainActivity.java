f2dad25bdb35d2f624baa4f0a6f14317c3c55db9
gooberbobber <dabamcdoodle@gmail.com>
2019-01-07 15:16:12 -0300

    Wip, make my variables only one character long so that they're anonymous and nobody can reverse-engineer my math >:)

--- app/src/main/java/arectangle/rectanglesinside/rectanglesinsidearectangle/MainActivity.java
+++ app/src/main/java/arectangle/rectanglesinside/rectanglesinsidearectangle/MainActivity.java
@@ -7,7 +7,20 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private EditText pieceLength, pieceHeight, canvasLength, canvasHeight;
-    private double pl, ph, cl, ch;
+    // In this case, the outer rectangle is the canvas, and the inner rect is the piece
+    // I may use hard brackets the way you would use parentheses in math, just to clarify complicated sentences.
+    private double
+            H, // Canvas height
+            W, // Canvas width
+            C, // Piece length
+            c, // Piece height
+            B, // Distance between the intersection of [canvas's top and piece's upper left corner] and [the upper right corner of the canvas]
+            T, // Horizontal distance between [the bottom left corner of the piece] and [the upper right corner of the canvas]
+            J, // Horizontal distance between piece's [upper left and bottom right] corners
+            D, // The piece's length along the diagonal, AKA the distance between the piece's [upper left and bottom right] corners
+            x, // The angle that the piece makes with the horizontal
+            y, // The angle that the line along the piece's diagonal makes with the horizontal sides of the piece
+            z; // Variables x and y combined
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +34,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onGoClick(View v) {
-        pl = Double.parseDouble(pieceLength.getText().toString());
-        ph = Double.parseDouble(pieceHeight.getText().toString());
-        cl = Double.parseDouble(canvasLength.getText().toString());
-        ch = Double.parseDouble(canvasHeight.getText().toString());
+        C = Double.parseDouble(pieceLength.getText().toString());
+        c = Double.parseDouble(pieceHeight.getText().toString());
+        W = Double.parseDouble(canvasLength.getText().toString());
+        H = Double.parseDouble(canvasHeight.getText().toString());
+
+        checkValidValues();
+
+        doStuff();
+    }
+
+    private void checkValidValues() {
+
+    }
+
+    private void doStuff() {
+
     }
 }
